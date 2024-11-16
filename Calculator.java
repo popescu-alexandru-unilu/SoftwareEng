270cdabf1cc61b68d7d844a29428306e7d2c5c1b
popescu-alexandru-unilu <145783546+popescu-alexandru-unilu@users.noreply.github.com>
2024-11-16 14:38:53 +0100

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,7 @@ package Quiz4;
         }
 
         public int subtract(int a, int b) {
-            return a - b;
+            return a - "b";
         }
 
         public int multiply(int a, int b) {
